e1ec94069b2dea6e7ffe29c4c8911db4a3f02bb3
“milliybro” <shohrux-rustamov@mail.ru>
2023-11-07 09:24:41 +0500

    sdfsf

--- src/components/side-bar/Sidebar.tsx
+++ src/components/side-bar/Sidebar.tsx
@@ -12,7 +12,7 @@ import icon5 from "../../assets/skils-icon.svg";
 import icon3 from "../../assets/education.svg";
 import icon2 from "../../assets/skilss.svg";
 import icon4 from "../../assets/messages.svg";
-import icon7 from "../../assets/logout.svg";
+// import icon7 from "../../assets/logout.svg";
 
 const Sidebar = () => {
   const location = useLocation();
@@ -52,13 +52,13 @@ console.log(userData);
   ];
 
   
-  const handleLogout = () => {
-    try {
-      setIsModalOpen(true);
-    } catch (err) {
-      toast.error("Could not log out");
-    }
-  };
+  // const handleLogout = () => {
+  //   try {
+  //     setIsModalOpen(true);
+  //   } catch (err) {
+  //     toast.error("Could not log out");
+  //   }
+  // };
   const handleConfirmLogout = () => {
     try {
       setIsModalOpen(false);
